521dcd92bc808bac6b036bc14983e5e831e8e00d
Daniel Marcovich <danielmark999@gmail.com>
2022-11-14 20:28:02 -0500

    update createNumberArr and getRandom

--- scripts/intro.js
+++ scripts/intro.js
@@ -2,16 +2,17 @@ const form = document.querySelector('.btnContainer');
 const inputName = document.querySelector('.input-playerName');
 const playBtn = document.querySelector('.btn-playerName');
 
+// Valida selector y crea evento
 if(playBtn){
     playBtn.addEventListener('click', getInputName);
 }
 
-// clear storage
+// Limpia input
 export function clearStorage(){
     sessionStorage.removeItem('newPlayer')
 }
 
-// safe input data
+// Almacena datos del input
 function getInputName() {
     clearStorage();
     sessionStorage.setItem('newPlayer', inputName.value)
